3ce60718d11a33d5fc713050a4181909ddf01dad
Wilendr <wilendrs@gmail.com>
2025-03-25 21:32:11 +0100

    Update CubeAnalyser.bat

--- example/CubeAnalyser.bat
+++ example/CubeAnalyser.bat
@@ -1,4 +1,13 @@
 @echo off
 cd /d "%~dp0"
+
+where python >nul 2>nul
+if errorlevel 1 (
+    echo Python is not installed on this system.
+    echo Please install Python 3.8 or later from https://www.python.org/downloads/
+    pause
+    exit /b
+)
+
 python program/main.py
 pause
